e0a6c5ecdf898e287a51d8ae498b779bd33255d4
Yiyang Zhu <ererandkeke@gmail.com>
2018-01-10 15:21:08 -0500

    recall finalize() and demonstrate garbage collection work

---
+++ Finalize.java
@@ -0,0 +1,11 @@
+class Finalize{
+    public static void main(String[] args){
+        int count;
+        
+        FDemo ob = new FDemo(0);
+        
+        for(count = 1; count < 500000; count++){
+            ob.generator(count);
+        }
+    }
+}
