ab444dc5e11593f5ab32dcc1505c76c095c0043c
Yiyang Zhu <ererandkeke@gmail.com>
2018-01-11 22:41:28 -0500

    demonstrate Queue.jav

---
+++ QDemo.java
@@ -0,0 +1,31 @@
+public class QDemo{
+    public static void main(String[] args){
+        Queue bigQ = new Queue(100);
+        Queue smallQ = new Queue(4);
+        char ch;
+        int i;
+        
+        System.out.println("Using bigQ to store the alphabet.");
+        for(i = 0; i < 26;i++){
+            bigQ.put((char)('A'+i));
+        }
+        System.out.println("Contents of bigQ: ");
+        for(i = 0; i < 26; i++){
+            System.out.print(bigQ.get()+" ");
+        }
+        
+        System.out.println("Using smallQ to generate errors.");
+        for(i = 0; i < 7; i++){
+            System.out.print("Attempting to store "+(char)('Z'-i));
+            smallQ.put((char)('Z' - i));
+            System.out.println();
+        }
+        
+        System.out.println("Contents on smallQ: ");
+        for(i = 0; i < 7;i++){
+            System.out.print(smallQ.get()+" ");
+        }
+    }
+}
+    
+    
