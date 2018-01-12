8b74d53908bae275ccbd8dc5d19e369dce0aae3d
Yiyang Zhu <ererandkeke@gmail.com>
2018-01-12 17:27:25 -0500

    demonstrate how to use bitwise operator &

--- UpCase.java
+++ UpCase.java
@@ -2,8 +2,9 @@ class UpCase{
     public static void main(String[] args){
         char ch, sh;
         
-        for(int i = 0; i < 10; i++){
+        for(int i = 0; i < 27; i++){
             ch = (char)('a'+i);
+            //95 and 223 work as well
             sh = (char)(ch & 65503);
             System.out.print(ch+" "+sh+"\t");
         }
