6b4bb93e1ae5c0df5f79ec058f9afd848650e19c
Yiyang Zhu <ererandkeke@gmail.com>
2018-01-13 16:53:25 -0500

    demonstrate that Integer -128~127 are stored as the same object

---
+++ DiffInt.java
@@ -0,0 +1,10 @@
+public class DiffInt{
+    public static void main(String[] args){
+        Integer a = 10000;
+        Integer b = 10000;
+        Integer c = -128;
+        Integer d = -128;
+        System.out.println("a and b are the same? "+(a==b));
+        System.out.println("c and d are the same? "+(c==d));
+    }
+}
