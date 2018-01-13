bd5d5ce8b32d438efccaa910f03a892f592beb03
Yiyang Zhu <ererandkeke@gmail.com>
2018-01-12 22:12:35 -0500

    demonstrate how to use XOR to encode and decode

--- Encode.java
+++ Encode.java
@@ -3,19 +3,20 @@ public class Encode{
         String message = "This is a test.";
         String encodeMessage = "";
         String decodeMessage = "";
-        int key = 66;
+        String key = "thisis";
+        int l = key.length();
         
         System.out.println("The original message is: "+message);
         
         for(int i = 0; i < message.length();i++){
-            char letter = (char)(message.charAt(i) ^ key);
+            char letter = (char)(message.charAt(i) ^ key.charAt(i%l));
             encodeMessage += letter;
         }
         
         System.out.println("The encoded message is: "+encodeMessage);
         
         for(int i = 0; i < encodeMessage.length();i++){
-            char letter = (char)(encodeMessage.charAt(i) ^ key);
+            char letter = (char)(encodeMessage.charAt(i) ^ key.charAt(i%l));
             decodeMessage += letter;
         }
         
